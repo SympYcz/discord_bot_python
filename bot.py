0e112e4fef115c11ae3bdfa470c2341a64d1ce9f
SympY <79143961+SympYcz@users.noreply.github.com>
2021-02-16 14:47:57 +0100

    Update bot.py
    Custom status added

--- bot.py
+++ bot.py
@@ -5,8 +5,8 @@ client = commands.Bot(command_prefix=">")
 
 @client.event
 async def on_ready():
-    await client.change_presence(status=discord.Status.idle, activity=discord.Game('My prefix is >'))
-    print("Bot is ready")
+     await client.change_presence(status=discord.Status.idle, activity=discord.Game('My prefix is >'))
+     print("Bot is ready")
 
 @client.command()
 async def hello(ctx):
