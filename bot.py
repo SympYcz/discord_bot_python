0758747c5955f3456656fae84772d75a4a98a0c1
SympY <79143961+SympYcz@users.noreply.github.com>
2021-02-16 14:47:27 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,6 +5,7 @@ client = commands.Bot(command_prefix=">")
 
 @client.event
 async def on_ready():
+    await client.change_presence(status=discord.Status.idle, activity=discord.Game('My prefix is >'))
     print("Bot is ready")
 
 @client.command()
