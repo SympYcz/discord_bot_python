4943c54a57abf0e84a767af41d4e9b667970b707
SympY <79143961+SympYcz@users.noreply.github.com>
2021-02-16 14:31:46 +0100

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,15 @@
+import discord
+from discord.ext import commands
+
+client = commands.Bot(command_prefix=">")
+
+@client.event
+async def on_ready():
+    print("Bot is ready")
+
+@client.command()
+async def hello(ctx):
+    await ctx.send("Hi")
+
+
+client.run("Token here")
